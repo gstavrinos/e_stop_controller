9b37b364a47312345f5c76f534a05efa9ad5c5d6
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-02-02 11:24:38 +0200

    Reflect new package name changes, + minor fixes

--- scripts/estop.py
+++ scripts/estop.py
@@ -10,20 +10,21 @@ twist_publisher = None
 def imu_callback(msg):
     global pitch_threshold, twist_publisher, cnt
     (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
-    print "PITCH = " + str(pitch)
+    #print "PITCH = " + str(pitch)
     if pitch < pitch_threshold and abs(pitch_threshold-pitch) >= 0.01:
-        print 'D A N G E R ! ! !'
+        #print 'D A N G E R ! ! !'
         twist_publisher.publish(Twist())
-	#print "PITCH = " + str(pitch)
+    '''
     else:
         print 'SAFE :)'
+    '''
 
 def init():
     global pitch_threshold, twist_publisher
-    rospy.init_node('assisted_teleoperation')
+    rospy.init_node('e_stop_controller')
     imu_topic = rospy.get_param('~imu_topic','/imu/data')
-    pitch_threshold = rospy.get_param('~pitch_threshold', -1.16)
-    twist_topic = rospy.get_param('~twist_topic','/assisted_teleop/cmd_vel')
+    pitch_threshold = rospy.get_param('~pitch_threshold', -0.7)
+    twist_topic = rospy.get_param('~twist_topic','/e_stop')
     rospy.Subscriber(imu_topic, Imu, imu_callback)
     pitch_threshold = -0.7
     twist_publisher = rospy.Publisher(twist_topic, Twist, queue_size=10);
